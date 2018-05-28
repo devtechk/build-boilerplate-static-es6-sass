702a2abcbb5a63f1ca4d81f4202298c40a54ac72
antonio giuliano <antoninogiuliano.mail@gmail.com>
2018-05-28 18:00:45 +0200

    init apple music website

--- source/index.js
+++ source/index.js
@@ -1,15 +1,40 @@
 // import $ from 'jquery'; Before using jQuery, install it with `npm install --save jquery`
+
+
+import $ from 'jquery' ;
 import './more-javascript/more.js';
 import 'normalize.css'; // Note this
 import './style/style.sass';
 import './style/style.css';
+import Swiper from 'swiper';
 
-const saySomething = (something) => {
-  console.log(something); // eslint-disable-line no-console
-};
+$(document).ready(function () {
+    $('#menu-toggle').click(function(){
 
-saySomething('Something! (index.js)');
+        $('.menu-list').toggleClass('clicked');
 
+    });
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        freeMode: true,
+        noSwiping: true,
+        noSwipingClass: 'swiper-no-swiping',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+          768: {
+              noSwiping: false
+          }
+        },
+    })
+});
+
+console.log('indexJS');
 /*
 To use jQuery, first install it as a dependency: `npm install --save jquery`.
 Then include `import $ from 'jquery';` at the top every JavaScript file that uses jQuery.
